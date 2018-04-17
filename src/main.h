4e2060e56606e4f26970fd44f7ab46a5da3bd9cc
zero24x <zero@you-spam.com>
2018-04-17 21:48:39 +0200

    fix wrong fee

--- src/main.h
+++ src/main.h
@@ -61,7 +61,7 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 /** Default for -maxorphanblocks, maximum number of orphan blocks kept in memory */
 static const unsigned int DEFAULT_MAX_ORPHAN_BLOCKS = 750;
 /** Fees smaller than this (in satoshi) are considered zero fee (for transaction creation) */
-static const int64_t MIN_TX_FEE = 1000000;
+static const int64_t MIN_TX_FEE = 100000000;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 /** No amount larger than this (in satoshi) is valid */
